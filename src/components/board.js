2e4939b0c656e277ae74b5d022751acfbb4613e7
Harry Angelo Soer <gelo.soer@gmail.com>
2018-10-07 04:11:52 +0800

    Improved the rendering method of grids

--- src/components/board.js
+++ src/components/board.js
@@ -2,30 +2,19 @@ import React from 'react';
 import Square from './square';
 
 class Board extends React.Component {
+  
   renderSquare(i) {
     return <Square
       data={this.props.squares[i]}
       onClick={() => this.props.onClick(i)}
-     />;
+    />;
   }
 
   render() {
     return (
       <div>
         <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
+          {[0, 1, 2, 3, 4, 5, 6, 7, 8].map(gridNumber => this.renderSquare(gridNumber))}
         </div>
       </div>
     );
